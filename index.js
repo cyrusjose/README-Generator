24724654332a50668e1a4db31eba57eb00e81cbd
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-13 16:34:38 -0700

    Added prompts for user

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
+const promptUser = ()=>{
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is your project about?'
+        }
+    ])
 }
 
-// function to initialize program
-function init() {
+// array of questions for user
+const questions = [];
 
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+async function init() {
+  console.log("Hello!");
+  
 }
 
 // function call to initialize program
