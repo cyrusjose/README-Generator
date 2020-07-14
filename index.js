203cb131f87210817030fe9201a31d77857a696b
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-13 17:11:28 -0700

    Populated questions array

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ const promptUser = ()=>{
             type: 'input',
             name: 'description',
             message: 'What is your project about?'
+        },
+        {
+            type: 'input'
         }
     ])
 }
 
 // array of questions for user
-const questions = [];
+const questions = ['What is the title of your project?', 'What is your project about?', 'What are your installation requirements?', 'How do use this application?', 'What license would you like to use?', 'Are you open to contributions?(If you are, please provide your requirements)', 'What command will run your test?', 'Enter your GitHub username.', 'Enter a valid email for your users to reach you.' ];
 
 // function to write README file
 function writeToFile(fileName, data) {}
