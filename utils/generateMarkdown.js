76e3459cee2803fa9df764b40b09974e5bb236be
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-13 18:58:47 -0700

    testing license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -30,7 +30,7 @@ function generateMarkdown(answers) {
  ${answers.testInstructions}
  ${'```'}
  ## License
- ${badges()}
+ ${badges(choice)}
  ## Questions
  If you have any further questions please feel free to reach out to me at any of the following: ${'\n'}
  github.com/${answers.githubUser}
